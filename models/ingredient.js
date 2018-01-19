a0928af7499abc43d179ab1698e9cba7e50fc2b5
dy46 <dy46@duke.edu>
2018-01-19 01:08:30 -0500

    created new ingredient model

---
+++ models/ingredient.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+
+var IngredientSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	package: {
+		type: String,
+		enum: ['Sack', 'Pail', 'Drum', 'Supersack', 'Truckload', 'Railcar'],
+		required: true,
+		trim: true
+	},
+	temperature: {
+		type: String,
+		enum: ['frozen', 'refrigerated', 'room temperature'],
+		required: true,
+		trim: true
+	}
+})
+
+var Ingredient = mongoose.model('Ingredient', IngredientSchema);
+module.exports = Ingredient;
