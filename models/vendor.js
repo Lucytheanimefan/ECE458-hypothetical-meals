2d7948de7e24334ab91f761e03fcc0b75272706c
Teddy Franceschi <ttf6@duke.edu>
2018-01-19 19:56:33 -0500

    adds model for vendors

---
+++ models/vendor.js
@@ -0,0 +1,56 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+
+
+var VendorSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required: true,
+  },
+  code:{
+    type:String,
+    required: true,
+    unique: true
+  },
+  contact:{
+    type:Mixed,
+    required: true
+  },
+  catalogue: [{food:String, units:{sack:{cost:Number,avail:Number},
+    pail:{cost:Number,avail:Number},supersack:{cost:Number,avail:Number},
+    truckload:{cost:Number,avail:Number},railcar:{cost:Number,avail:Number}}}]
+});
+//authenticate input against database
+
+VendorSchema.methods.findByCode = function(code, callback){
+  Vendor.findOne({code: code})
+    .exec(function(err, vendor){
+      if(err){
+        return callback(err)
+      }else if(!vendor){
+        var err = new Error('No Vendor Found with that Code.');
+        err.status = 401;
+        return callback(err);
+      }
+      return vendor;
+    });
+}
+
+VendorSchema.methods.findByName = function(name, callback){
+  Vendor.findOne({name: name})
+    .exec(function(err,vendor){
+      if(err){
+        return callback(err)
+      }else if(!vendor){
+        var err = new Error('No Vendor Found with that Name');
+        err.status = 401;
+        return callback(err);
+      }
+      return vendor;
+    })
+}
+
+
+var Vendor = mongoose.model('Vendor', VendorSchema);
+module.exports = Vendor;
