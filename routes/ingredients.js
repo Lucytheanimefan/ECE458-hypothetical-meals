cedabde0bc4dfba2d18ddc749ef7c8f8ef828a61
dy46 <dy46@duke.edu>
2018-01-20 01:05:34 -0500

    changed indent to spaces

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -4,27 +4,28 @@ var Ingredient = require('../models/ingredient');
 
 //GET request to show available ingredients
 router.get('/', function(req, res) {
-	res.render('ingredient');
+  res.render('ingredient');
 })
 
 //POST request to create a new ingredient
 router.post('/new', function(req, res, next) {
-	Ingredient.create({
-		name: req.body.name,
-		package: req.body.package,
-		temperature: req.body.temperature
-	}, function (error, newInstance) {
-		if (error) {
-			return next(error);
-		} else {
-			//alert user the ingredient has been successfully added.
-		}
-	});
+  Ingredient.create({
+    name: req.body.name,
+    package: req.body.package,
+    temperature: req.body.temperature,
+    amount: req.body.amount
+  }, function (error, newInstance) {
+    if (error) {
+      return next(error);
+    } else {
+      //alert user the ingredient has been successfully added.
+    }
+  });
 });
 
 //PUT request to update an existing ingredient
-// router.put('/update', function(req, res) {
-// 	Ingredient.findOneAndUpdate({name: req.body.name}, function());
-// })
+router.put('/update', function(req, res) {
+  Ingredient.findOneAndUpdate({name: req.body.name}, function(){});
+});
 
 module.exports = router;
