e5a400eda7aa6369b14a5420e91fbd2a58355e85
Teddy Franceschi <ttf6@duke.edu>
2018-01-24 20:25:39 -0500

    routes work for single vvendor search, update, delete, need to build front end for multi search and fix that route

--- models/vendor.js
+++ models/vendor.js
@@ -1,26 +1,4 @@
 var mongoose = require('mongoose');
-<<<<<<< HEAD
-var bcrypt = require('bcrypt');
-
-var VendorSchema = new mongoose.Schema({
-  name:{
-    type:String,
-    required: true,
-  },
-  code:{
-    type:String,
-    required: true,
-    unique: true
-  },
-  contact:{
-    type:String,
-    required: true
-  },
-  location:{
-    type:String,
-    required: true
-=======
-
 
 var VendorSchema = new mongoose.Schema({
   name: {
