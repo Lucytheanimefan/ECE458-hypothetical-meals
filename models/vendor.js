67ddfe47fedaa7342b0600312d8a58f9b954f3dd
Teddy Franceschi <ttf6@duke.edu>
2018-01-29 14:54:14 -0500

    basic inventory

--- models/vendor.js
+++ models/vendor.js
@@ -5,21 +5,62 @@ var VendorSchema = new mongoose.Schema({
   name: {
     type: String,
     unique: true,
-    required: true
+    required: true,
+    trim: true
   },
   code: {
     type: String,
     unique: true,
     required: true,
+    trim: true
   },
   contact: {
     type: String,
     required: true,
+    trim: true
   },
   location:{
-    type: String,
-    require: true
-  }
+    city:{
+      type:String,
+      required:true,
+      trim: true
+    },
+    state:{
+      type:String,
+      required:true,
+      trim: true
+    }
+  },
+  catalogue:[{
+    ingredient: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    units:{
+      sack: {cost:Number,available:Number},
+      pail: {cost:Number,available:Number},
+      drum: {cost:Number,available:Number},
+      supersack: {cost:Number,available:Number},
+      truckload: {cost:Number,available:Number},
+      railcar: {cost:Number,available:Number}
+    }
+
+  }],
+  history:[{
+    ingredient:{
+      type:String,
+      required:true
+    },
+    units:{
+      sack:Number,
+      pail:Number,
+      drum:Number,
+      supersack:Number,
+      truckload:Number,
+      railcar:Number
+    }
+  }]
 })
 
 var Vendor = mongoose.model('Vendor', VendorSchema);
--- routes/vendors.js
+++ routes/vendors.js
@@ -5,17 +5,6 @@ var uniqid = require('uniqid')
 
 //GET request to show available ingredients
 router.get('/', function(req, res) {
-  /*
-  Vendor.find({}, function(error, vendors) {
-    if (error) {
-      var err = new Error('Error loading vendors ');
-      err.status = 400;
-      return next(err);
-    } else {
-      //res.render('ingredients', { ingredients: ings });
-      res.render('vendor');
-    }
-  })*/
   res.render('vendors');
 })
 
@@ -51,34 +40,27 @@ router.post('/:code/delete', function(req, res, next) {
 
 
 router.post('/:code/update', async function(req, res, next) {
-  var uicode;
-  await Vendor.findOne({code: req.params.code}, function(error, ing) {
-    if (ing == null) {
-      var err = new Error('That vendor doesn\'t exist!');
-      err.status = 404;
-      return next(err);
-    } else if (error) {
-      var err = new Error('Error searching for ' + req.params.code);
-      err.status = 400;
-      return next(err);
-    } else {
-      uicode = ing.code;
-    }
-  })
-  await Vendor.findOneAndUpdate({code: req.params.code}, {$set: {
-    name: req.body.name,
-    contact: req.body.contact,
-    location: req.body.location,
-    code: uicode
-  }}, function(error, result) {
-    if (error) {
-      var err = new Error('Couldn\'t update that vendor.');
-      err.status = 400;
-      return next(err);
-    } else {
-      return res.redirect(req.baseUrl + '/' + uicode);
-    }
+  Vendor.findOne({code: req.params.code}, function(err, vendor){
+  if (err) {
+    var error = new Error('Couldn\'t find that vendor.');
+    error.status = 400;
+    return next(error);
+  }
+    vendor.name = req.body.name;
+    vendor.contact = req.body.contact;
+    location = genLocation(req.body);
+    catalogue = genCatalogue(req.body,vendor.catalogue);
+    history = [];
+  vendor.save(function(err) {
+    if (err) {
+      var error = new Error('Couldn\'t update that vendor.');
+      error.status = 400;
+      return next(error);
+      }
+    });
+  return res.redirect(req.baseUrl + '/' + req.params.code);
   });
+
 });
 
 //POST request to create a new ingredient
@@ -88,7 +70,8 @@ router.post('/new', function(req, res, next) {
     name: req.body.name,
     code: newid,
     contact: req.body.contact,
-    location: req.body.location
+    location: genLocation(req.body),
+    catalogue: []
   }, function (error, newInstance) {
     if (error) {
       return next(error);
@@ -99,10 +82,27 @@ router.post('/new', function(req, res, next) {
   });
 });
 
+router.post('/:code/order', function(req,res,next){
+  let quantity = req.body.quantity;
+  let size = req.body.size;
+  let ingredient = req.body.ingredient;
+  if(checkFridge(quantity,'cold')){
+    Vendor.findOne({code: req.params.code}, function(err, vendor){
+    if (err) { return next(err); }
+      if(vendor['catalogue'][0]['units'][size]['available'] >= quantity){
+        vendor['catalogue'][0]['units'][size]['available'] -= quantity;
+      }
+      vendor.save(function(err) {
+        if (err) { return next(err); }
+      });
+    });
+  }
+});
+
 router.get('/search', function(req, res, next){
   let ingredient = req.body.ingredient;
   let name = req.body.name;
-  let location = req.body.location;
+  let location = genLocation(req.body);
   let code = req.body.code;
   let contact = req.body.contact;
   Vendor.find({code:code, name:name, contact:contact, location:location},
@@ -112,15 +112,31 @@ router.get('/search', function(req, res, next){
       err.status = 400;
       return next(err);
     } else {
-      //res.render('ingredients', { ingredients: ings });
-      //res.render('vendor');
     }
   })
 })
 
-//PUT request to update an existing ingredient
-// router.post('/update/', function(req, res) {
-//   Ingredient.findOneAndUpdate({name: req.body.name}, function(){});
-// });
+genLocation = function(data){
+  var loc = {};
+  loc['city']=data['city'];
+  loc['state']=data['state'];
+  return loc;
+}
+
+genCatalogue = function(data,catalogue){
+  console.log(catalogue);
+  var entry = {};
+  entry.ingredient = data.ingredient;
+  entry.units = {};
+  entry.units[data['container']]={};
+  entry.units[data['container']]['cost']=parseFloat(data.cost);
+  entry.units[data['container']]['available']=parseFloat(data.available);
+  catalogue.push(entry);
+  return catalogue;
+}
+
+checkFridge = function(size,temp){
+  return true;
+}
 
 module.exports = router;
