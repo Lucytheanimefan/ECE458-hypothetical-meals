29cebe02c8c1da60078eebd1638c2846bdcd9967
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-01-30 12:47:26 -0500

    Fixed issue where authentication doesn't work for anyone except admin. Removed code in User model in .pre('save'). Resolve issue #28

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ var nodemailer = require('nodemailer');
 var underscore = require('underscore');
 
 var config = require('../env.json');
-
+var bcrypt = require('bcrypt');
 
 
 /* GET users listing. */
@@ -41,16 +41,15 @@ router.post('/', function(req, res, next) {
   if (req.body.email &&
     req.body.username &&
     req.body.password &&
-    req.body.passwordConf) {
-
-    var userRole = req.body.role ? req.body.role : "User";
+    req.body.passwordConf &&
+    req.body.role) {
 
     var userData = {
       email: req.body.email,
       username: req.body.username,
       password: req.body.password,
       passwordConf: req.body.passwordConf,
-      role: userRole,
+      role: req.body.role,
     }
 
     User.create(userData, function(error, user) {
@@ -58,44 +57,60 @@ router.post('/', function(req, res, next) {
         console.log("Error creating user");
         return next(error);
       } else {
-        console.log("Create token");
-        // Create a verification token for this user
-        var token = new Token({ _userId: user._id, token: crypto.randomBytes(16).toString('hex') });
-
-        // Save the verification token
-
-        token.save(function(err) {
+        console.log('Hash the password');
+        bcrypt.hash(user.password, 10, function(err, hash) {
           if (err) {
-            console.log("Error saving token");
-            return res.status(500).send({ msg: err.message });
+            return next(err);
           }
 
-          console.log("Send the email for account confirmation");
-          // Send the email
-          var transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            auth: {
-              user: config["email"],
-              pass: config["password"]
-            }
-          });
-          var mailOptions = {
-            from: 'spothorse9.lucy@gmail.com',
-            to: user.email,
-            subject: 'Account Verification Token',
-            text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' +
-              req.headers.host + '\/users\/confirmation?id=' + token.token + '.\n'
-          };
+          console.log('Successful hash');
+          user.password = hash;
+          user.save(function(err) {
+            if (err) { return res.status(500).send({ msg: err.message }); }
 
-          transporter.sendMail(mailOptions, function(err) {
-            if (err) {
-              console.log("Error sending email");
-              return res.status(500).send({ msg: err.message });
-            }
-            res.status(200).render(index, { title: 'A verification email has been sent to ' + user.email + '.' });
+            console.log("Create token");
+            // Create a verification token for this user
+            var token = new Token({ _userId: user._id, token: crypto.randomBytes(16).toString('hex') });
+
+            // Save the verification token
+
+            token.save(function(err) {
+              if (err) {
+                console.log("Error saving token");
+                return res.status(500).send({ msg: err.message });
+              }
+
+              console.log("Send the email for account confirmation");
+              // Send the email
+              var transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 465,
+                auth: {
+                  user: config["email"],
+                  pass: config["password"]
+                }
+              });
+              var mailOptions = {
+                from: 'spothorse9.lucy@gmail.com',
+                to: user.email,
+                subject: 'Account Verification Token',
+                text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' +
+                  req.headers.host + '\/users\/confirmation?id=' + token.token + '.\n'
+              };
+
+              transporter.sendMail(mailOptions, function(err) {
+                if (err) {
+                  console.log("Error sending email");
+                  return res.status(500).send({ msg: err.message });
+                }
+                res.status(200).render(index, { title: 'A verification email has been sent to ' + user.email + '.' });
+              });
+            });
+
           });
-        });
+
+        })
+
 
       }
     });
@@ -236,7 +251,7 @@ router.get('/isAdmin', function(req, res, next) {
     .exec(function(error, user) {
       var isAdmin = (!error && user !== null && user.role.toUpperCase() === "ADMIN");
       console.log('isAdmin: ' + isAdmin);
-      res.send({'isAdmin': isAdmin});
+      res.send({ 'isAdmin': isAdmin });
     });
 });
 
@@ -312,7 +327,8 @@ router.post('/addtocart', function(req, res, next) {
       User.create({
         _id: req.session.userId,
         cart: {
-          [ingredient]: quantity }
+          [ingredient]: quantity
+        }
       }, function(err, cart_instance) {
         if (err) return next(err);
         return res.redirect(req.baseUrl + '/cart');
