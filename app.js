c8e3e7025cd47d4d316993fe1f621b136837839f
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-02-01 18:04:05 -0500

    Began work on uploading, parsing and reading in CSV line by line

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var ingredients = require('./routes/ingredients');
 var vendors = require('./routes/vendors');
+var files = require('./routes/files');
 var MongoStore = require('connect-mongo')(session);
 
 //var seed = require('./helpers/seed.js');
@@ -66,7 +67,7 @@ app.use('/users',users);
 app.use('/ingredients', users.requireLogin(),ingredients); //This is not ideal
 app.post('/ingredients/*', users.requireRole("admin"), ingredients);
 app.use('/vendors', users.requireRole("admin"), vendors);
-
+app.use('/files', files);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
