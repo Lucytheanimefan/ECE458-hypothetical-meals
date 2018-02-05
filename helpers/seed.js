2c633f8eedf7668f8651111e8e62eee07d8929a2
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-02-04 19:01:00 -0500

    Fixed seed to create current inventory default values of 0

--- helpers/seed.js
+++ helpers/seed.js
@@ -10,37 +10,37 @@ var Inventory = require('../models/inventory');
 var EMAIL = (process.env.EMAIL) ? process.env.EMAIL : require('../env.json')['email'];
 var PASSWORD = (process.env.PASSWORD) ? process.env.PASSWORD : require('../env.json')['password'];
 
-var user = {
-  username: 'Admin User',
-  email: 'admin@admin.com',
-  role: 'Admin',
-  password: 'admin',
-  passwordConf: 'admin',
-  isVerified: true
-}
+// var user = {
+//   username: 'Admin User',
+//   email: 'admin@admin.com',
+//   role: 'Admin',
+//   password: 'admin',
+//   passwordConf: 'admin',
+//   isVerified: true
+// }
 
-User.create(user, function(error, user) {
-  if (error) {
-    console.log('Error creating user: ' + error);
-    return;
-  } else {
-    console.log('Hash the password');
-    bcrypt.hash(user.password, 10, function(err, hash) {
-      if (err) {
-        return;
-      }
+// User.create(user, function(error, user) {
+//   if (error) {
+//     console.log('Error creating user: ' + error);
+//     return;
+//   } else {
+//     console.log('Hash the password');
+//     bcrypt.hash(user.password, 10, function(err, hash) {
+//       if (err) {
+//         return;
+//       }
 
-      console.log('Successful hash');
-      user.password = hash;
-      user.save(function(err) {
-        if (err) { return res.status(500).send({ msg: err.message }); }
+//       console.log('Successful hash');
+//       user.password = hash;
+//       user.save(function(err) {
+//         if (err) { return res.status(500).send({ msg: err.message }); }
 
-        console.log('Done');
+//         console.log('Done');
 
-      });
-    });
-  };
-})
+//       });
+//     });
+//   };
+// })
 
 var inventory = { 
   type:'master',
