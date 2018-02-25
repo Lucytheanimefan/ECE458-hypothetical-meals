f7f3c1ab1d2fec09d3a33fdaa640fda2eb77db8a
dy46 <dy46@duke.edu>
2018-02-25 17:59:04 -0500

    basic reporting

--- routes/reports.js
+++ routes/reports.js
@@ -24,21 +24,53 @@ router.get('/:page', function(req, res, next) {
     spendingReport = results[0];
     productionReport = results[1];
     formulaReport = results[2];
-    console.log(spendingReport.spending);
-    console.log(productionReport.spending);
-    console.log(formulaReport.product);
-    res.render('report', {spending: spendingReport.spending, production: productionReport.spending, formula: formulaReport.product});
+    spendingPromise = Promise.all(spendingReport.spending.map(function(tuple) {
+      return getIngredientName(tuple);
+    }));
+    productionPromise = Promise.all(productionReport.spending.map(function(tuple) {
+      return getIngredientName(tuple);
+    }));
+    formulaPromise = Promise.all(formulaReport.product.map(function(tuple) {
+      return getFormulaName(tuple);
+    }));
+    return Promise.all([spendingPromise, productionPromise, formulaPromise]);
+  }).then(function(results) {
+    res.render('report', {spending: results[0], production: results[1], formula: results[2]});
   }).catch(function(error) {
     next(error);
   })
 })
 
+getFormulaName = function(tuple) {
+  return new Promise(function(resolve, reject) {
+    var entry = {};
+    entry['totalCost'] = tuple.totalCost;
+    entry['unitsProduced'] = tuple.unitsProduced;
+    Formula.model.findById(mongoose.Types.ObjectId(tuple.formulaId)).then(function(formula) {
+      if (formula == null) {
+        entry['formula'] = tuple.formulaName;
+      } else {
+        entry['formula'] = formula.name;
+      }
+      return entry;
+    }).then(function(entry) {
+      resolve(entry);
+    }).catch(function(error) {
+      reject(error);
+    });
+  });
+}
+
 getIngredientName = function(tuple) {
   return new Promise(function(resolve, reject) {
     var entry = {};
     entry['totalSpent'] = tuple.totalSpent;
-    Ingredient.getIngredientById(mongoose.Types.ObjectId(tuple.ingredient)).then(function(ing) {
-      entry['ingredient'] = ing.name;
+    Ingredient.getIngredientById(mongoose.Types.ObjectId(tuple.ingredientId)).then(function(ing) {
+      if (ing == null) {
+        entry['ingredient'] = tuple.ingredientName;
+      } else {
+        entry['ingredient'] = ing.name;
+      }
       return entry;
     }).then(function(entry) {
       resolve(entry);
